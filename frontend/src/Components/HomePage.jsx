68755a64fe3eb6bba554219d2cc7fb9818b399d3
jabelop <jabelop@hotmail.com>
2021-07-16 12:06:37 -0400

    - Table data fetch, fixed

--- frontend/src/Components/HomePage.jsx
+++ frontend/src/Components/HomePage.jsx
@@ -1,19 +1,12 @@
-import React , { useState, useEffect }from "react";
+import React from "react";
 import { ProcessTable } from "./UIComponents/ProcessTable";
 import axios from "axios";
 
 export function HomePage() {
-  const [data, setData] = useState({ hits: [] });
-  useEffect(async () => {
-    const result = await axios.get(
-      'http://localhost/api/process');
- 
-    setData(result.data);
-  });
   return (
     <div>
-      <h1>Home Page!</h1>
-      <ProcessTable processes={data.hits}></ProcessTable>
+      <h1>Proccesses</h1>
+      <ProcessTable></ProcessTable>
     </div>
   );
 }
--- frontend/src/Components/UIComponents/ProcessTable.jsx
+++ frontend/src/Components/UIComponents/ProcessTable.jsx
@@ -1,45 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 import axios from "axios";
 
-export class ProcessTable extends React.Component {
-    render() {
-      return (
-        <Table striped bordered hover>
-            <thead>
-                <tr>
-                <th>Process Id</th>
-                <th>Process Type</th>
-                <th>Process Result</th>
-                <th>Process Status</th>
-                <th>Created At</th>
-                <th>Started At</th>
-                <th>Finished At</th>
-                <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                {this.props.processes.map(process => 
-                <tr>
-                <td>{process.id}</td>
-                <td>{process.type}</td>
-                <td>{process.result}</td>
-                <td>{process.status}</td>
-                <td>{process.created_at}</td>
-                <td>{process.started_at}</td>
-                <td>{process.finished_at}</td>
-                <td><Button onClick={() => this.handleClick(process.id)}>Start</Button></td>
-                </tr>
-                )}
-            </tbody>
-        </Table>
-      );
-    }
-    async handleClick(id) {
-      const result = axios.post(`http://localhost/api/process/${id}/start`);
-      
-    }
-  }
+export function ProcessTable (props) {
+  const handleClick = async (id) => {
+    const result = await axios.post(`http://localhost/api/process/${id}/start`);
+    
+  };
+  const [data, setData] = useState([]);
+  useEffect(async () => {
+    const result = await axios.get(
+      'http://localhost/api/process');
+    console.log(result);
+    setData(result.data);
+  }, []);
+  return (
+    <Table striped bordered hover>
+        <thead>
+            <tr>
+            <th>Process Id</th>
+            <th>Process Type</th>
+            <th>Process Output</th>
+            <th>Process Status</th>
+            <th>Created At</th>
+            <th>Started At</th>
+            <th>Finished At</th>
+            <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            {data?.map(process => 
+            <tr>
+            <td>{process.process_id}</td>
+            <td>{process.type}</td>
+            <td>{process.output}</td>
+            <td>{process.status}</td>
+            <td>{process.created_at}</td>
+            <td>{process.started_at}</td>
+            <td>{process.finished_at}</td>
+            <td><Button onClick={() => handleClick(process.process_id)}>Start</Button></td>
+            </tr>
+            )}
+        </tbody>
+    </Table>
+  );
+}
 
   
